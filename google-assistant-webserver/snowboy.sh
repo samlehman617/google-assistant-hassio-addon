4440cbfcae0b5851b25fbecd4ae7f41126c5f091
Sam Lehman <samlehman617@gmail.com>
2018-11-27 17:10:58 -0500

    Inserted debugging statements in bash scripts.

--- google-assistant-webserver/snowboy.sh
+++ google-assistant-webserver/snowboy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+echo "[INFO] Creating project dir..."
 # Make project dir
 make -p /Development/Assistant
 cd ~/Development/Assistant
-
+echo "[INFO] Cloning snowboy repo..."
 # Clone repo
 git clone https://github.com/Makerspot/snowboy.git
 
+echo "[INFO] Compiling swig for Raspbian Stretch."
 # Recompile for Raspbian Stretch
 sudo ln -s /usr/bin/swig3.0 /usr/local/bin/swig
 cd ~/Development/Assistant/snowboy/swig/Python3
