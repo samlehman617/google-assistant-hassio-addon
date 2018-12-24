95136d5a4a48ef1a88f030603e7be670d4ac4cd8
Sam Lehman <samlehman617@gmail.com>
2018-12-23 21:40:58 -0500

    Moved paths in snowboy.sh to root dir.

--- google-assistant-webserver/snowboy.sh
+++ google-assistant-webserver/snowboy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "[INFO] Creating project dir..."
+
 # Make project dir
+echo "[INFO] Creating project dir..."
 make -p /Development/Assistant
-cd ~/Development/Assistant
-echo "[INFO] Cloning snowboy repo..."
+mkdir -p /Development/Assistant
+cd /Development/Assistant
+
 # Clone repo
+echo "[INFO] Cloning snowboy repo..."
 git clone https://github.com/Makerspot/snowboy.git
 
-echo "[INFO] Compiling swig for Raspbian Stretch."
 # Recompile for Raspbian Stretch
+echo "[INFO] Compiling swig for Raspbian Stretch."
 sudo ln -s /usr/bin/swig3.0 /usr/local/bin/swig
-cd ~/Development/Assistant/snowboy/swig/Python3
+cd /Development/Assistant/snowboy/swig/Python3
 make
 cd /
